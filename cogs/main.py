758ba64b10a91419743ccd6c158a3d28a042425c
Humble <70359789+HumbleToS@users.noreply.github.com>
2022-11-17 20:19:54 -0700

    Start for a cog (so the folder will be pushed to the repo

---
+++ cogs/main.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+import discord
+
+class Main(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if self.bot.user.mention == message.content or f'<@!{self.bot.user.id}>' == message.content:
+            em = discord.Embed(
+                description=f'Hello! My here prefix is `?`',
+                color=discord.Color.random(),
+            )
+            await message.reply(embed=em)
+        else:
+            return
+
+async def setup(bot):
+    await bot.add_cog(Main(bot))
