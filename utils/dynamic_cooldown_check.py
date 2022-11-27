5fc2a672f2581488d5d6cc682891613ef3de18c4
Humble <70359789+HumbleToS@users.noreply.github.com>
2022-11-27 14:09:25 -0700

    Black Formatting

--- utils/dynamic_cooldown_check.py
+++ utils/dynamic_cooldown_check.py
@@ -3,6 +3,7 @@ from discord import app_commands
 
 from typing import Optional
 
+
 def owner_cooldown_bypass(interaction: discord.Interaction) -> Optional[app_commands.Cooldown]:
     """Uses the applications team members to indicate whether the command should return a cooldown or not
 
