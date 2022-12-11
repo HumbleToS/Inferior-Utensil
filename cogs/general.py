5c84833b226efa12a32424b63444f6fae09479a7
Robert Drescher <robert@macombhometeam.com>
2022-12-10 22:16:19 -0500

    Add cleanup command

--- cogs/general.py
+++ cogs/general.py
@@ -20,6 +20,30 @@ class Main(commands.Cog):
             )
             await message.reply(embed=em)
 
+    @commands.command()
+    @commands.guild_only()
+    @commands.cooldown(1, 5.0, commands.BucketType.user) # 1 per 5 seconds per user
+    async def cleanup(self, ctx: commands.Context, amount: int=100):
+        """Purges messages from and relating to the bot.
+
+        Parameters
+        ----------
+        amount : int, optional
+            The number of messages to check (1-100), defaults to 100.
+        """
+        amount = max(min(amount, 100), 1)
+
+        async with ctx.typing():
+            can_purge_author = ctx.channel.permissions_for(ctx.guild.me).manage_messages
+            try:
+                channel_prefixes = tuple(await self.bot.get_prefix(ctx.message))
+                msgs = await ctx.channel.purge(limit=amount, check=lambda m: (m.author == self.bot.user and len(m.components) == 0) or (can_purge_author and m.author == ctx.author and m.content.startswith(channel_prefixes)))
+                await ctx.send(f"Removed {len(msgs)} messages.", delete_after=10.0)
+
+            except (discord.Forbidden, discord.HTTPException):
+                await ctx.send("I couldn't process this request. Please check my permissions.")
+
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Main(bot))
